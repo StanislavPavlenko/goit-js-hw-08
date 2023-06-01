a1d264875324b2f6b4277920cb0bc28568288310
StanislavPavlenko <voltfrag@gmail.com>
2023-06-02 01:50:44 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'; 
 
+const iframe = document.querySelector('iframe');
+const playerIframe = new Player(iframe);
+
+let currentTime;
+const trottleTimeupdatePlayer = throttle(onTimeupdate, 1000);
+
+playerIframe.on('timeupdate', trottleTimeupdatePlayer);
+function onTimeupdate(e) {
+currentTime = e.seconds;
+localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  iframePlayer.setCurrentTime(savedTime);
+}
